82f780006bfdb698c560852ddeb69042045deefb
Кристина <kristina.ezikova@yandex.ru>
2020-10-23 23:48:11 +0300

    Add endMinutesLessThanStartMinutesTest test

--- src/test/java/day12/TimeTests.java
+++ src/test/java/day12/TimeTests.java
@@ -8,29 +8,38 @@ import static org.hamcrest.Matchers.is;
 
 public class TimeTests {
     @Test
-    public void timeTest(){
-        String startTime="10:30";
-        String endTime="15:40";
+    public void timeTest() {
+        String startTime = "10:30";
+        String endTime = "15:40";
 
-        String timeStr = TimeHelper.time(startTime,endTime);
+        String timeStr = TimeHelper.time(startTime, endTime);
         assertThat(timeStr, is("5:10"));
     }
 
     @Test
-    public void endTimeLessThanStartTimeTest(){
-        String startTime="15:30";
-        String endTime="11:40";
+    public void endTimeLessThanStartTimeTest() {
+        String startTime = "15:30";
+        String endTime = "11:40";
 
-        String timeStr = TimeHelper.time(startTime,endTime);
+        String timeStr = TimeHelper.time(startTime, endTime);
         assertThat(timeStr, is("20:10"));
     }
 
     @Test
-    public void endTimeEqualToStartTimeTest(){
-        String startTime="15:30";
-        String endTime="15:30";
+    public void endTimeEqualToStartTimeTest() {
+        String startTime = "15:30";
+        String endTime = "15:30";
 
-        String timeStr = TimeHelper.time(startTime,endTime);
+        String timeStr = TimeHelper.time(startTime, endTime);
         assertThat(timeStr, is("0:0"));
+    }
+
+    @Test
+    public void endMinutesLessThanStartMinutesTest() {
+        String startTime = "15:30";
+        String endTime = "15:20";
+
+        String timeStr = TimeHelper.time(startTime, endTime);
+        assertThat(timeStr, is("23:50"));
     }
 }
